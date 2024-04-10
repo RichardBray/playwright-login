4c6759c3a8179e4b1c7a86476a0e07b90f744b97
RichardBray <richardbray89@gmail.com>
2024-04-10 16:55:21 +0100

    feat: simulate site down

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Login } from "./components/Login.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Dashboard } from "./components/Dashboard.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-]);
+const router = null;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <h1 style={{ padding: "3rem" }}>Site is down 😔</h1>
   </React.StrictMode>
 );
